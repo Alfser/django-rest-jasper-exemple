d386739ec7a92aa9a586aef4003eaf676a44b96c
janilson.serra <j.janilson12@gmail.com>
2024-04-17 23:22:26 -0300

    change column name of model pessoa.

--- pessoa/models.py
+++ pessoa/models.py
@@ -6,7 +6,7 @@ class Pessoa(models.Model):
     nome = models.CharField(verbose_name="Nome", max_length=100)
     rua = models.CharField(verbose_name="Rua", max_length=255)
     cidade = models.CharField(verbose_name="Cidade", max_length=100)
-    telefone= models.CharField(verbose_name="Telefone", max_length=20)
+    telfone= models.CharField(verbose_name="Telefone", max_length=20)
     
     class Meta:
         verbose_name = "Pessoa"    
